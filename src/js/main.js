84fc7ba01675a04cfca8e6c936cb4459ea936ceb
elisemutrux <122873450+elisemutrux@users.noreply.github.com>
2024-03-28 09:32:18 +0100

    fenêtre excercer

--- src/js/main.js
+++ src/js/main.js
@@ -3,3 +3,15 @@ var saveButton = document.querySelector(".save");
 saveButton.addEventListener("click", function () {
   saveButton.classList.add("is-open");
 });
+
+var excerciceOpen = document.querySelector(".excercer-button");
+var excerciceBox = document.querySelector(".excercice-body");
+var excerciceClose = document.querySelector(".excercice-close");
+
+excerciceOpen.addEventListener("click", function () {
+  excerciceBox.classList.add("is-open");
+});
+
+excerciceClose.addEventListener("click", function () {
+  excerciceBox.classList.remove("is-open");
+});
